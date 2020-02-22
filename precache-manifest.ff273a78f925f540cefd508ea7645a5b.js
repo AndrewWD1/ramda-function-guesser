821152bafdd0dc387874baa8e5f5b996805a634f
Andrew Doumont <andrew.doumont@gmail.com>
2020-02-22 22:37:04 +0900

    Updates

---
+++ precache-manifest.ff273a78f925f540cefd508ea7645a5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2bfdfa08c097573a198e7835b62ab1c5",
+    "url": "/ramda-function-guesser/index.html"
+  },
+  {
+    "revision": "aaa1b2866533742e19aa",
+    "url": "/ramda-function-guesser/static/css/main.9ea8a55d.chunk.css"
+  },
+  {
+    "revision": "f69cbdcbb96e911075f8",
+    "url": "/ramda-function-guesser/static/js/2.a1e0cadd.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/ramda-function-guesser/static/js/2.a1e0cadd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aaa1b2866533742e19aa",
+    "url": "/ramda-function-guesser/static/js/main.736be849.chunk.js"
+  },
+  {
+    "revision": "18a8896f61b1409c5c27",
+    "url": "/ramda-function-guesser/static/js/runtime-main.dcdf3afe.js"
+  }
+]);
